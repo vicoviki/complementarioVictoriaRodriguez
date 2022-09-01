2dadb43515509167e5ce00ffb6b7cb2f998171cd
victoria <vicoviki1994@gmail.com>
2022-09-01 15:39:24 -0300

    subida bucle js

--- main.js
+++ main.js
@@ -10,27 +10,6 @@
 
 // Crear un algoritmo utilizando un ciclo
 
-//  for(let i = 0; i <=3 ; i++){
-//     let nombre = prompt('Ingrese su nombre:');
-//     alert ('Hola '+ nombre );
-//     let edadIngresada = Number(prompt('Ingrese su edad:'));
-//     alert('Su edad es: '+edadIngresada);
-//     let resultado = edadIngresada + 1;
-//     alert('El año que viene tendrás '+resultado+' años');
-// }
-
-// for (let i=0 ; i<=15 ; i++){
-
-// }
-
-
-// let cantidad = Number(prompt('ingrese cantidad de prendas: '));
-// for(let i = 1; i <=10 ; i--){
-
-// let resultado = i--
-// alert(resultado)
-// }
-
 
 for (let i = 10 ; i => 0 ; i--) {
     let cantidad = Number(prompt('ingrese cantidad de prendas: '));
